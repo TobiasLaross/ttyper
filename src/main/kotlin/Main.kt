5dce00eadb46141fc441951617f8308725b268c0
Anton Öhlin <anton.ohlin@assaabloy.com>
2025-10-20 12:34:35 +0200

    added dictornary to generate random words on each run

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,30 @@
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import java.io.File
 
 fun main() {
     val terminal = DefaultTerminalFactory().createTerminal()
-    while (true) {
-        val key = terminal.readInput()
-        println("${key.character}")
+    val dictionary = readDictionary()
+    val wordsToType = dictionary.joinToString(separator = " ").toCharArray()
+    println(wordsToType)
+    for (char in wordsToType) {
+        var correctKeyPressed = false
+        while (!correctKeyPressed) {
+            val key = terminal.readInput()
+            if (key.character == char) {
+                print(key.character)
+                correctKeyPressed = true
+            }
+        }
     }
+}
+
+fun readDictionary(): List<String> {
+    val input = File("src/main/kotlin/dictionary")
+    val dictionary = input.readLines()
+    val numberOfWordsToPick = 30
+    val words = mutableListOf<String>()
+    repeat(numberOfWordsToPick) {
+        words.add(dictionary.random().lowercase())
+    }
+    return words
 }
