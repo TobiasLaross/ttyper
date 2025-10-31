eb0b4e751451c9ac0c088174f44255d739dddc17
Alexander Björck <alexander.bjorck@assaabloy.com>
2025-10-31 13:39:59 +0100

    optional build and run flag

--- run.sh
+++ run.sh
@@ -1,3 +1,26 @@
-./gradlew --no-daemon uberJar --console=plain
+#!/bin/sh
+
+build=0
+
+show_help() {
+  echo "./run.sh to run"
+  echo "./run.sh -b to build and run"
+}
+
+while getopts "h?b" opt; do
+  case "$opt" in
+    h)
+      show_help
+      exit 0
+      ;;
+    b)  build=1
+      ;;
+  esac
+done
+
+if [ $build -eq 1 ]; then
+  ./gradlew --no-daemon uberJar --console=plain
+fi
+
 java --enable-native-access=ALL-UNNAMED -jar build/libs/ttyper.jar
 
